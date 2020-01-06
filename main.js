5d1c5cf8ff0b1a9ea2f16658732ba7ab51e77a5c
Zendy Lim <agus.zendy@givery.co.jp>
2020-01-06 14:22:44 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Github CI Open in New Tab
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Open CI details in Github PR page in new tab
+// @author       Zendy Lim
+// @match        https://github.com/givery-technology/codecheck-backend/pull/*
+// @grant        none
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// ==/UserScript==
+
+function addTargetBlankAttributeToLinks(jNode) {
+  jNode.attr('target', '_blank');
+}
+
+waitForKeyElements('.status-actions', addTargetBlankAttributeToLinks);
+
